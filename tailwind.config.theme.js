d8b3c18ad3f2f27ae63a58ecdcc495a121cbfa65
anderslemke <mail@anderslemke.dk>
2021-09-20 15:20:05 +0200

    Initial commit

---
+++ tailwind.config.theme.js
@@ -0,0 +1,62 @@
+module.exports = {
+    colors: {
+      transparent: 'transparent',
+      current: 'currentColor',
+      inherit: 'inherit',
+      blue: {
+        night: '#203B62',
+        preusser: '#47729C',
+        sea: '#203B62',
+        grey: '#8ABCD9',
+        baby: '#BAD3F1',
+      },
+      green: {
+        copenhagen: '#06592F',
+        yes: '#25BBB0',
+      },
+      grey: {
+        dark: '#737882',
+        mid: '#D9D9D9',
+        light: '#F3F3F3',
+      },
+      yellow: {
+        bright: '#FFFFAC',
+        happy: '#F5CC5B',
+      },
+      'z-orange': '#FA4113',
+      turquoise: '#A1DDF7',
+      rose: '#E5C0D1',
+      terracotta: '#E0704E',
+      kardus: '#D0C2A8',
+      beige: '#BAAE95',
+      'band-aid': '#E8C7B6',
+      paper: '#FAF8F0',
+      black: '#231F20',
+      white: '#FFFFFF',
+      "dark-text": '#333333',
+    },
+    fontFamily: {
+      editorial: ['Italian Plate No2', 'Gotham SSm A', 'Gotham SSm B', 'Arial', 'Verdana', 'sans-serif'],
+      expanded: ['Italian Plate No2 Expanded', 'Gotham SSm A', 'Gotham SSm B', 'Arial', 'Verdana', 'sans-serif'],
+      operative: ['Italian Plate No2', 'Helvetica', 'Arial', 'Verdana', 'sans-serif'],
+      'body-copy': ['ff-meta-serif-web-pro', 'Georgia', 'Times', 'serif'],
+      'small-caps': ['ff-meta-serif-sc-web-pro', 'ff-meta-serif-web-pro', 'Georgia', 'Times', 'serif'],
+    },
+    extend: {
+      backgroundOpacity: {
+        95: '0.95',
+      },
+      borderWidth: {
+        3: '3px',
+      },
+      boxShadow: {
+        top: '0 -1px 3px 0 rgba(0, 0, 0, 0.1), 0 -1px 2px 0 rgba(0, 0, 0, 0.06)',
+      },
+      height: {
+        '80vh': '80vh',
+      },
+      width: {
+        80: '20rem',
+      },
+    },
+};
